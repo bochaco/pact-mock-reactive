80ee5692245ba4f828f523ca0f350950f6aad549
ezequielgarcia88 <ezequiel.garcia88@gmail.com>
2015-06-03 15:21:52 -0300

    Enable body comparator

--- server/pact-mock-reactive.js
+++ server/pact-mock-reactive.js
@@ -1,81 +1,83 @@
 Interactions = new Mongo.Collection('interactions');
 
 Meteor.startup(function () {
-  // code to run on server at startup
+    // code to run on server at startup
+    return;
 });
 
 Meteor.methods({
-  resetInteractions: function () {
-    Interactions.update({count: {$gte: 0}}, {$set : {count: 1}});
-    Interactions.remove({count: {$lt: 0}});
-  },
-  clearInteractions: function () {
-    Interactions.remove({});
-  }    
+    resetInteractions: function () {
+        Interactions.update({ count: { $gte: 0 } }, { $set : { count: 1 } });
+        Interactions.remove({ count: { $lt: 0 } });
+    },
+    clearInteractions: function () {
+        Interactions.remove({});
+    }
 });
 
-var fs = Npm.require('fs'),
-	appRoot = fs.realpathSync('.');
-// find better way	
+var path = Npm.require('path'),
+    appRoot = path.resolve('.');
+// find better way
 appRoot = appRoot.indexOf('.meteor') >= 0 ? appRoot.substring(0, appRoot.indexOf('.meteor')) : appRoot;
 
-var	registerInteraction = function (req, res) {
-    var selector = {
-        'interaction.request.method': req.body.request.method, 
-        'interaction.request.path': req.body.request.path,
-        'interaction.request.query': req.body.request.query,
-        'interaction.request.headers': req.body.request.headers
-    }
-    var matchingInteraction = Interactions.findOne(selector);
-    
-    var consumerName = req.headers['x-pact-consumer'];
-    var providerName = req.headers['x-pact-provider'];
-    if (matchingInteraction) {
-        if (consumerName != matchingInteraction.consumer || providerName != matchingInteraction.provider) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('The interaction is already registered but against a different pair of consumer-provider');
+var fs = Npm.require('fs'),
+    registerInteraction = function (req, res) {
+        var selector = {
+                'interaction.request.method': req.body.request.method,
+                'interaction.request.path': req.body.request.path,
+                'interaction.request.query': req.body.request.query,
+                'interaction.request.headers': req.body.request.headers
+            },
+            matchingInteraction = Interactions.findOne(selector),
+            consumerName = req.headers['x-pact-consumer'],
+            providerName = req.headers['x-pact-provider'];
+
+        if (matchingInteraction) {
+            if (consumerName !== matchingInteraction.consumer || providerName !== matchingInteraction.provider) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('The interaction is already registered but against a different pair of consumer-provider');
+            } else {
+                Interactions.update({ _id: matchingInteraction._id }, { $inc: { count: 1 } });
+            }
         } else {
-            Interactions.update({ _id: matchingInteraction._id }, { $inc: { count: 1 } });
+            Interactions.insert({
+                consumer: consumerName,
+                provider: providerName,
+                count: 1,
+                disabled: false,
+                interaction: req.body
+            });
         }
-    } else {
-        var newInteraction = {
-            consumer: consumerName,
-            provider: providerName,
-            count: 1,
-            disabled: false,
-            interaction: req.body
-        };
-        Interactions.insert(newInteraction);
-    }
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Set interactions for ' + consumerName + '-' + providerName);
-},
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Set interactions for ' + consumerName + '-' + providerName);
+    },
     verifyInteractions = function (res) {
-        var unexpectedReqs = Interactions.find({ count: { $lt: 0 }, disabled: false }).fetch();
-        var missingReqs = Interactions.find({ count: { $gt: 0 }, disabled: false }).fetch();
+        var unexpectedReqs = Interactions.find({ count: { $lt: 0 }, disabled: false }).fetch(),
+            missingReqs = Interactions.find({ count: { $gt: 0 }, disabled: false }).fetch(),
+            resText;
         if (missingReqs.length === 0 && unexpectedReqs.length === 0) {
             res.writeHead(200, { 'Content-Type': 'text/plain' });
             res.end('Interactions matched');
         } else {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
-            var resText = 'Actual interactions do not match expected interactions for mock MockService.\n';
+            resText = 'Actual interactions do not match expected interactions for mock MockService.\n';
             if (missingReqs.length > 0) {
                 resText += '\nMissing requests:\n';
-                for (var index in missingReqs) {
-                    resText += '\t' + missingReqs[index].consumer + '-' + missingReqs[index].provider + ': ';
-                    resText += missingReqs[index].interaction.request.method.toUpperCase() + ' ';
-                    resText += missingReqs[index].interaction.request.path;
-                    resText += ' (' + missingReqs[index].count + ' missing request/s)\n';
-                }
+                _.each(missingReqs, function (element) {
+                    resText += '\t' + element.consumer + '-' + element.provider + ': ';
+                    resText += element.interaction.request.method.toUpperCase() + ' ';
+                    resText += element.interaction.request.path;
+                    resText += ' (' + element.count + ' missing request/s)\n';
+                });
             }
             if (unexpectedReqs.length > 0) {
                 resText += '\nUnexpected requests:\n';
-                for (var index in unexpectedReqs) {
-                    resText += '\t' + unexpectedReqs[index].consumer + '-' + unexpectedReqs[index].provider + ': ';
-                    resText += unexpectedReqs[index].interaction.request.method.toUpperCase() + ' ';
-                    resText += unexpectedReqs[index].interaction.request.path;
-                    resText += ' (' + (-1 * unexpectedReqs[index].count) + ' unexpected request/s)\n';
-                }
+                _.each(unexpectedReqs, function (element) {
+                    resText += '\t' + element.consumer + '-' + element.provider + ': ';
+                    resText += element.interaction.request.method.toUpperCase() + ' ';
+                    resText += element.interaction.request.path;
+                    resText += ' (' + (-1 * element.count) + ' unexpected request/s)\n';
+                });
             }
             res.end(resText);
         }
@@ -84,24 +86,23 @@ var	registerInteraction = function (req, res) {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         if (req.body.consumer && req.body.consumer.name && req.body.provider && req.body.provider.name) {
             var interactions = Interactions.find({
-                consumer: req.body.consumer.name,
-                provider: req.body.provider.name,
-                disabled: false
-            }).fetch();
-            
-            var pact = req.body;
+                    consumer: req.body.consumer.name,
+                    provider: req.body.provider.name,
+                    disabled: false
+                }).fetch(),
+                pact = req.body,
+                filename = (req.body.consumer.name.toLowerCase() + '-' + req.body.provider.name.toLowerCase()).replace(/\s/g, '_'),
+                path = appRoot + 'pacts/' + filename + '.json';
+
             pact.interactions = [];
-            for (var index in interactions) {
-                pact.interactions.push(interactions[index].interaction);
-            }
-            
+            _.each(interactions, function (element) {
+                pact.interactions.push(element.interaction);
+            });
+
             pact.metadata = {
                 'pactSpecificationVersion': '1.0.0'
-            }
-            
-            var filename = (req.body.consumer.name.toLowerCase() + '-' + req.body.provider.name.toLowerCase()).replace(/\s/g, '_'),
-				path = appRoot + 'pacts/' + filename + '.json';
-            
+            };
+
             fs.writeFile(path, JSON.stringify(pact), function (err) {
                 if (err) {
                     res.end(JSON.stringify({ 'message': 'Error ocurred in mock service: RuntimeError - pact file couldn\'t be saved' }));
@@ -113,70 +114,50 @@ var	registerInteraction = function (req, res) {
             res.end(JSON.stringify({ 'message': 'Error ocurred in mock service: RuntimeError - You must specify a consumer and provider name' }));
         }
     },
-    isObjContained = function (obj1, obj2, twoway) {
-        var result = true;
-        for (var prop in obj1) {
-            if (typeof obj2[prop] == 'undefined') {
-                return false;
-            }
-            
-            if (typeof obj2[prop] == 'object') {
-                if (!isObjContained(prop, obj2[prop])) {
-                    return false;
-                }
-            }
-            
-            if (obj1[prop] != obj2[prop]) {
-                return false;
-            }
-        }
-        if (twoway) {
-            result = isObjContained(obj2, obj1, false);
-        }
-        return result;
-    },
     requestsHandler = function (method, path, query, req, res) {
         var selector = {
-            'interaction.request.method': method.toLowerCase(), 
-            'interaction.request.path': path,
-            disabled: false
-        }
-        var matchingInteractions = Interactions.find(selector).fetch();
-        var done = false;
-        var err = {
-            'message': 'No interaction found for ' + method + ' ' + path + ' ' + JSON.stringify(query),
-            'interaction_diffs': []
-        }
-        
+                'interaction.request.method': method.toLowerCase(),
+                'interaction.request.path': path,
+                disabled: false
+            },
+            matchingInteractions = Interactions.find(selector).fetch(),
+            err = {
+                'message': 'No interaction found for ' + method + ' ' + path + ' ' + JSON.stringify(query),
+                'interaction_diffs': []
+            },
+            matchingHeaders,
+            expectedResponse;
+
         if (matchingInteractions.length > 1) { // this shouldn't occur
             err.message = 'Multiple interaction found for ' + method + ' ' + path;
-            for (var index in matchingInteractions) {
+            _.each(matchingInteractions, function (element) {
                 err.interaction_diffs.push({
-                    description: matchingInteractions[index].interaction.description,
-                    request: matchingInteractions[index].interaction.request
+                    description: element.interaction.description,
+                    request: element.interaction.request
                 });
-            }
+            });
             res.writeHead(500, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(err));
-            done = true;
+            return;
         }
-        else if (matchingInteractions.length === 1) {
+        if (matchingInteractions.length === 1) {
             // compare headers of actual and expected
-            var matchingHeaders = matchingInteractions[0].interaction.request.headers;
-            for (var key in matchingHeaders) { // iterate thru expected headers to make sure it's in the actual header
+            matchingHeaders = matchingInteractions[0].interaction.request.headers;
+            // iterate thru expected headers to make sure it's in the actual header
+            _.each(matchingHeaders, function (value, key) {
                 var actualHdr = req.headers[String(key).toLowerCase()];
-                if (!actualHdr || actualHdr !== matchingHeaders[key]) {
+                if (!actualHdr || actualHdr !== value) {
                     err.interaction_diffs.push({
                         'headers': {
-                            'expected': { key: key, value: matchingHeaders[key] },
+                            'expected': { key: key, value: value },
                             'actual': { key: key, value: actualHdr }
                         }
                     });
                 }
-            }
-            
+            });
+
             // compare query of actual and expected
-            if (false == isObjContained(matchingInteractions[0].interaction.request.query, req.query, true)) {
+            if (!_.isEqual(matchingInteractions[0].interaction.request.query, req.query)) {
                 err.interaction_diffs.push({
                     'query': {
                         'expected': matchingInteractions[0].interaction.request.query,
@@ -184,38 +165,44 @@ var	registerInteraction = function (req, res) {
                     }
                 });
             }
-            
-            /** TODO verification of body is missing  **/
-            
+
+            // compare body of actual and expected
+            if (!_.isEqual(matchingInteractions[0].interaction.request.body, req.body)) {
+                err.interaction_diffs.push({
+                    'body': {
+                        'expected': matchingInteractions[0].interaction.request.body,
+                        'actual': req.body
+                    }
+                });
+            }
+
             if (err.interaction_diffs.length === 0) {
                 Interactions.update({ _id: matchingInteractions[0]._id }, { $inc: { count: -1 } });
-                var expectedResponse = matchingInteractions[0].interaction.response;
+                expectedResponse = matchingInteractions[0].interaction.response;
                 res.writeHead(expectedResponse.status, expectedResponse.headers);
                 res.end(JSON.stringify(expectedResponse.body));
-                done = true;
+                return;
             }
         }
-        
-        if (!done) {   // this is an unexpected interaction
-            var newInteraction = {
-                consumer: 'UNEXPECTED',
-                provider: 'UNEXPECTED',
-                count: -1,
-                interaction: {
-                    request: {
-                        method: method.toLowerCase(),
-                        path: path,
-                        query: query,
-                        headers: req.headers,
-                        body: req.body
-                    },
-                    reponse: {}
-                }
-            };
-            Interactions.insert(newInteraction);
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(err));
-        }
+
+        // this is an unexpected interaction
+        Interactions.insert({
+            consumer: 'UNEXPECTED',
+            provider: 'UNEXPECTED',
+            count: -1,
+            interaction: {
+                request: {
+                    method: method.toLowerCase(),
+                    path: path,
+                    query: query,
+                    headers: req.headers,
+                    body: req.body
+                },
+                reponse: {}
+            }
+        });
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(err));
     },
     routeInteractions = function (route) {
         var headers = route.request.headers;
@@ -223,9 +210,9 @@ var	registerInteraction = function (req, res) {
             registerInteraction(route.request, route.response);
         } else {
             requestsHandler(route.method, route.url, route.params.query, route.request, route.response);
-        } 
+        }
     },
-	routePact = function (route) {
+    routePact = function (route) {
         var headers = route.request.headers;
         if (headers['x-pact-mock-service'] === 'true') {
             writePact(route.request, route.response);
@@ -233,19 +220,19 @@ var	registerInteraction = function (req, res) {
             requestsHandler(route.method, route.url, route.params.query, route.request, route.response);
         }
     };
-	
+
 Router.route('/interactions', { where: 'server' })
     .post(function () {
         routeInteractions(this);
     })
-    .put(function () {
+        .put(function () {
         routeInteractions(this);
     })
     .delete(function () {
         var headers = this.request.headers;
         if (headers['x-pact-mock-service'] === 'true') {
             Interactions.remove({});
-            this.response.writeHead(200, {'Content-Type': 'text/plain'});
+            this.response.writeHead(200, { 'Content-Type': 'text/plain' });
             this.response.end('Deleted interactions');
         } else {
             requestsHandler(this.method, this.url, this.params.query, this.request, this.response);
@@ -266,15 +253,15 @@ Router.route('/pact', { where: 'server' })
     .post(function () {
         routePact(this);
     })
-    .put(function () {
+        .put(function () {
         routePact(this);
     });
 
 Router.route('(.+)', function () {
-	if(this.url === '/favicon.ico') {
-		this.response.writeHead(200, {'Content-Type': 'application/json'})
-		this.response.end();
-	} else {
-		requestsHandler(this.method, '/' + this.params, this.params.query, this.request, this.response);
-	}
-}, { where: 'server'});
+    if (this.url === '/favicon.ico') {
+        this.response.writeHead(200, { 'Content-Type': 'application/json' });
+        this.response.end();
+    } else {
+        requestsHandler(this.method, '/' + this.params, this.params.query, this.request, this.response);
+    }
+}, { where: 'server' });
